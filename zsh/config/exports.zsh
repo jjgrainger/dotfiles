c8776023f1236885445ce141864cf83cb8367562
jjgrainger <github@jjgrainger.co.uk>
2022-12-28 15:33:02 +0000

    update zsh config

--- zsh/config/exports.zsh
+++ zsh/config/exports.zsh
@@ -1,9 +1,28 @@
 # Add Homebrew sbin
 export PATH="/usr/local/sbin:$PATH"
 
+
+# PHP versions
+export PATH="/usr/local/opt/php@7.4/bin:$PATH"
+export PATH="/usr/local/opt/php@7.4/sbin:$PATH"
+
 # Add global Composer packages
 export PATH=$HOME/.composer/vendor/bin:$PATH
 
 # add rbenv to path
 export PATH="$HOME/.rbenv/bin:$PATH"
 eval "$(rbenv init -)"
+
+# Add mysql
+export PATH="/usr/local/opt/mysql@5.7/bin:$PATH"
+
+# nodenv
+eval "$(nodenv init -)"
+
+# Include global node modules
+export NODE_PATH='/usr/local/lib/node_modules'
+
+# Setup NVM, must be last.
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
